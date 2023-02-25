2c65326e0c71ab03be1d4656c0d31ca9876b6c2b
Alexandre Gomes <119986066+alefgomes0@users.noreply.github.com>
2023-02-25 12:28:51 -0300

    Add functions

--- guessingGame.js
+++ guessingGame.js
@@ -1,4 +1,4 @@
-function computerGuess() {
+function getComputerChoice() {
 	let computerPlay;
 	let computerNumber = Math.random();
 
@@ -16,7 +16,7 @@ function computerGuess() {
 }
 
 
-function userPlay() {
+function getUserChoice() {
 	while(true) {
 		let choice = prompt("Select an option: ").toLowerCase().trim();
 		if (choice === "rock" || choice === "paper" || choice === "scisor") {
@@ -25,3 +25,39 @@ function userPlay() {
 	}
 }
 
+function playRound(playerSelection, computerSelection) {
+	let versus = [playerSelection, computerSelection];
+	let phrase;
+	let whoWon;
+
+	if (playerSelection.length == computerSelection.length) {
+		phrase = "It's a draw!";
+	}
+	else if (versus.includes("paper", "rock")) {
+		phrase = "Paper beats rock!";
+		whoWon = "paper";
+	}
+	else if (versus.includes("rock", "scisor")) {
+		phrase = "Rock beats scisor!";
+		whoWon = "rock";
+	}
+	else if (versus.includes("paper", "scisor")) {
+		phrase = "Scisor beats paper!";
+		whoWon = "scisor";
+	}
+
+	return [whoWon, phrase];
+}
+
+
+	function findWinner() {
+	if (playerSelection == whoWon) {
+		return `You won! ${phrase}`;
+	}
+	else if(computerSelection == whoWon) {
+		return `You lost! ${phrase}`;
+	}
+	else {
+		return "It's a drawn!";
+	}
+}
