34e547fca6e7be1501453642c183f185e86f8cca
Alexandre Gomes <119986066+alefgomes0@users.noreply.github.com>
2023-02-25 14:21:43 -0300

    Add functions and edit program structure

--- guessingGame.js
+++ guessingGame.js
@@ -18,14 +18,16 @@ function getComputerChoice() {
 
 function getUserChoice() {
 	while(true) {
-		let choice = prompt("Select an option: ").toLowerCase().trim();
+		let choice = prompt("Select rock, paper or scisor: ").toLowerCase().trim();
 		if (choice === "rock" || choice === "paper" || choice === "scisor") {
 			return choice;
 		}
 	}
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
+	let playerSelection = getUserChoice();
+	let computerSelection = getComputerChoice();
 	let versus = [playerSelection, computerSelection];
 	let phrase;
 	let whoWon;
@@ -45,19 +47,70 @@ function playRound(playerSelection, computerSelection) {
 		phrase = "Scisor beats paper!";
 		whoWon = "scisor";
 	}
-
-	return [whoWon, phrase];
+	//Returns the result of computation as an array
+	return [playerSelection, computerSelection, whoWon, phrase];
 }
 
 
-	function findWinner() {
-	if (playerSelection == whoWon) {
-		return `You won! ${phrase}`;
+function findWinner() {
+	//Use return value from playRound() to find winner
+	//with array indexes
+	let results = playRound();
+	if (results[0] == results[2]) {
+		return `You won! ${results[3]}`;
 	}
-	else if(computerSelection == whoWon) {
-		return `You lost! ${phrase}`;
+	else if(results[1] == results[2]) {
+		return `You lost! ${results[3]}`;
 	}
 	else {
 		return "It's a drawn!";
+	}
+}
+
+
+function getRoundsNumber() {
+	let n = prompt("Enter number of rounds: ");
+	n = +n;
+	if (n <= 0 || n > 99) {
+		alert("Too many/few rounds");
+	}
+	else if (n == undefined) {
+		n = 1;
+	}
+
+	return Math.round(n);
+}
+
+function game() {
+	let playerWinCount = 0;
+	let computerWinCount = 0;
+	let n = getRoundsNumber();
+
+	for (let i = 0; i < n; i++) {
+		let play = findWinner();
+		console.log(play);
+
+		if (play.includes("won")) {
+			playerWinCount++;
+			if (playerWinCount > n/2) {
+				break;
+			}
+		}
+		else if (play.includes("lost")) {
+			computerWinCount++;
+			if (computerWinCount > n/2) {
+				break;
+			}
+		}
+	}
+
+	if (playerWinCount > computerWinCount) {
+		return "You won the game!";
+	}
+	else if (computerWinCount > playerWinCount) {
+		return "You lost the game!";
+	}
+	else {
+		return "It's a drawn";
 	}
 }
